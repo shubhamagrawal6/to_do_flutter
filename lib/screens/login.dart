240482c454734d142c4f3914ac13240d0b50b934
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-03-21 23:29:55 +0530

    made the login screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,110 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:to_do_flutter/services/auth.dart';
+
+class Login extends StatefulWidget {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+
+  const Login({
+    Key key,
+    @required this.auth,
+    @required this.firestore,
+  }) : super(key: key);
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("To-Do List",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Builder(builder: (BuildContext context) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  key: const ValueKey("username"),
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(labelText: "Username"),
+                  controller: _emailController,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  key: const ValueKey("password"),
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(labelText: "Password"),
+                  controller: _passwordController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // ignore: deprecated_member_use
+                RaisedButton(
+                  key: const ValueKey("signIn"),
+                  color: Colors.indigoAccent,
+                  onPressed: () async {
+                    final String retVal = await Auth(auth: widget.auth).signIn(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    HapticFeedback.lightImpact();
+                    if (retVal == "Success") {
+                      _emailController.clear();
+                      _passwordController.clear();
+                    } else {
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(retVal),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text("Sign In"),
+                ),
+                FlatButton(
+                  key: const ValueKey("createAccount"),
+                  onPressed: () async {
+                    final String retVal =
+                        await Auth(auth: widget.auth).createAccount(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    HapticFeedback.lightImpact();
+                    if (retVal == "Success") {
+                      _emailController.clear();
+                      _passwordController.clear();
+                    } else {
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(retVal),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
