6a77b3fe1daf61f339c619e58d1dda27df40c983
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-03-21 16:32:07 +0530

    addded error and loading screen

---
+++ lib/screens/loading.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 4,
+              ),
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 15.0,
+                ),
+              ]),
+          child: const Center(
+              child: Material(
+            type: MaterialType.transparency,
+            child: Text(
+              "Loading: \nPlease Wait",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ))),
+    );
+  }
+}
